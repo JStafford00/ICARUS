13a5177835f4ce9c976964baa92690336e8566a8
JStafford00 <42542054+JStafford00@users.noreply.github.com>
2021-09-03 19:14:22 -0500

    Add project files.

---
+++ DeskApp/ItemDisplay.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Icarus.Data;
+
+namespace DeskApp
+{
+    /// <summary>
+    /// Interaction logic for ItemDisplay.xaml
+    /// </summary>
+    public partial class ItemDisplay : UserControl
+    {
+        private Item thisItem = new Item();
+
+        public ItemDisplay()
+        {
+            InitializeComponent();
+            this.DataContext = new List<Item>();
+        }
+
+        private void SkuBox_KeyChange(object sender, TextChangedEventArgs e)
+        {
+            string s = ((TextBox)e.Source).Text;
+            if(s != "")
+            {
+                s = Regex.Replace(s, "[^0-9]", "");
+                ((TextBox)e.Source).Text = s;
+            }
+        
+        }
+
+        private void PopulateFields()
+        {
+            SkuBox.Text = thisItem.Sku.ToString();
+        }
+
+        private void PopulateItem()
+        {
+            thisItem.Sku = Int32.Parse(SkuBox.Text);
+        }
+
+        private void NewItemButton_Click(object sender, RoutedEventArgs e)
+        {
+            thisItem = new Item();
+            PopulateFields();
+        }
+
+        private void AddItemButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void DeleteItemButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Icarus/Item.cs
@@ -0,0 +1,304 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Icarus.Data
+{
+    /*
+     * Author: Jordan Stafford
+     * Class: Item.cs
+     * Purpose: To hold informaiton on general items.
+     */
+    public class Item
+    {
+        private int sku;
+        private string name;
+        private int overallQuantity;
+        private int salesFloorQuantity;
+        private int overstockQuantity;
+        private int prepaidQuantity;
+        private int maxSalesFloor;
+        private DateTime dateRecieved;
+        private DateTime dateInventoried;
+        private string inventoriedTeamMember;
+        private List<string> locationStocked;
+        private int departmentID;
+        private string departmentName;
+        private string departmentGroup;
+        private string catagory;
+        private string subcatagory;
+        private List<string> vendors;
+
+        /// <summary>
+        /// Sku number for an item.
+        /// </summary>
+        public int Sku
+        {
+            get
+            {
+                return sku;
+            }
+
+            set
+            {
+                sku = value;
+            }
+        }
+
+        /// <summary>
+        /// Name of the item.
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        /// <summary>
+        /// Overall quantity in sales floor and overstock.
+        /// </summary>
+        public int OverallQuantity
+        {
+            get
+            {
+                return overallQuantity;
+            }
+
+            set
+            {
+                overallQuantity = value;
+            }
+        }
+
+        /// <summary>
+        /// Quantity on the Sales Floor.
+        /// </summary>
+        public int SalesFloorQuantity
+        {
+            get
+            {
+                return salesFloorQuantity;
+            }
+
+            set
+            {
+                salesFloorQuantity = value;
+            }
+        }
+
+        /// <summary>
+        /// Quantity in Overstock.
+        /// </summary>
+        public int OverstockQuantity
+        {
+            get
+            {
+                return overstockQuantity;
+            }
+
+            set
+            {
+                overstockQuantity = value;
+            }
+        }
+
+        /// <summary>
+        /// Quantity on prepaid.
+        /// </summary>
+        public int PrepaidQuantity
+        {
+            get
+            {
+                return prepaidQuantity;
+            }
+
+            set
+            {
+                prepaidQuantity = value;
+            }
+        }
+
+        /// <summary>
+        /// Max Quantity that can Go on the Sales Floor.
+        /// </summary>
+        public int MaxSalesFloor
+        {
+            get
+            {
+                return maxSalesFloor;
+            }
+
+            set
+            {
+                maxSalesFloor = value;
+            }
+        }
+
+        /// <summary>
+        /// Date last recieved.
+        /// </summary>
+        public DateTime DateRecieved
+        {
+            get
+            {
+                return dateRecieved;
+            }
+
+            set
+            {
+                dateRecieved = value;
+            }
+        }
+
+        /// <summary>
+        /// Date last inventoried.
+        /// </summary>
+        public DateTime DateInventoried
+        {
+            get
+            {
+                return dateInventoried;
+            }
+
+            set
+            {
+                dateInventoried = value;
+            }
+        }
+
+        /// <summary>
+        /// Team member that inventoried item last.
+        /// </summary>
+        public string InventoriedTeamMember
+        {
+            get
+            {
+                return inventoriedTeamMember;
+            }
+
+            set
+            {
+                inventoriedTeamMember = value;
+            }
+        }
+
+        /// <summary>
+        /// Location of Stocked Item
+        /// </summary>
+        public List<string> LocationStocked
+        {
+            get
+            {
+                return locationStocked;
+            }
+
+            set
+            {
+                locationStocked = value;
+            }
+        }
+
+        /// <summary>
+        /// Department ID number.
+        /// </summary>
+        public int DepartmentID
+        {
+            get
+            {
+                return departmentID;
+            }
+
+            set
+            {
+                departmentID = value;
+            }
+        }
+
+        /// <summary>
+        /// Department name.
+        /// </summary>
+        public string DepartmentName
+        {
+            get
+            {
+                return departmentName;
+            }
+
+            set
+            {
+                departmentName = value;
+            }
+        }
+
+        /// <summary>
+        /// Department Group
+        /// </summary>
+        public string DepartmentGroup
+        {
+            get
+            {
+                return departmentGroup;
+            }
+
+            set
+            {
+                departmentGroup = value;
+            }
+        }
+
+        /// <summary>
+        /// Department Caragory
+        /// </summary>
+        public string Catagory
+        {
+            get
+            {
+                return catagory;
+            }
+
+            set
+            {
+                catagory = value;
+            }
+        }
+
+        /// <summary>
+        /// Department Subcatagory.
+        /// </summary>
+        public string Subcatagory
+        {
+            get
+            {
+                return subcatagory;
+            }
+
+            set
+            {
+                subcatagory = value;
+            }
+        }
+
+        /// <summary>
+        /// List of vendors
+        /// </summary>
+        public List<string> Vendors
+        {
+            get
+            {
+                return vendors;
+            }
+
+            set
+            {
+                vendors = value;
+            }
+        }
+    }
+}
